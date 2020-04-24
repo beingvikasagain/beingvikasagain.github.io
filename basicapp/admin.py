f07923c8ac66d081980d71f4a16ab34d7ebb369d
beingvikasagain <57279623+beingvikasagain@users.noreply.github.com>
2020-04-24 09:24:16 +0530

    changes

--- basicapp/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from basicapp.models import MyModel
-# Register your models here.
-admin.site.register(MyModel)
--- basicapp/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-from basicapp import views
-urlpatterns = [
-    path('',views.form_view,name='forms'),
-]
